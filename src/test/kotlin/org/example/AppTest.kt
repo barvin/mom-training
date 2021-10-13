eb1f761c4d296570ec585ff68863066e2f0b03c0
Maksym Barvinskyi <maksym@mbarvinskyi.com>
2021-10-13 09:45:52 +0300

    Formatted the file

--- src/test/kotlin/org/example/AppTest.kt
+++ src/test/kotlin/org/example/AppTest.kt
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 import java.time.Duration.ofSeconds
 import java.util.*
 
